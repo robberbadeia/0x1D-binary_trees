400ca5edc1f371a83f217238675992cb9180bde4
RobberBadeia <robber.badeia@gmail.com>
2024-01-02 22:39:35 +0200

    T08

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - Function
+ * @tree: input
+ * @func: Return
+ * Return: NONE
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
