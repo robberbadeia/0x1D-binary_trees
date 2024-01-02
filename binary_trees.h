a27b7ceebbbe113764c47ba0de04b610621035ad
RobberBadeia <robber.badeia@gmail.com>
2024-01-02 21:44:12 +0200

    HDR

--- binary_trees.h
+++ binary_trees.h
@@ -33,5 +33,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 /* Function that inserts a node as the right-child of another node*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-
+/* Function that deletes an entire binary tree */
+void binary_tree_delete(binary_tree_t *tree);
+/*  Function that checks if a node is a leaf */
+int binary_tree_is_leaf(const binary_tree_t *node);
+/* Function that checks if a given node is a root */
+int binary_tree_is_root(const binary_tree_t *node);
 #endif
