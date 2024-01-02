4a152b0322e87350f9d327dd1cae7748c78432d1
RobberBadeia <robber.badeia@gmail.com>
2024-01-02 21:38:25 +0200

    T04

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - Entry point
+ * @node: input
+ * Return: Always 1 (Success)
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+	{
+		return (0);
+	}
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
