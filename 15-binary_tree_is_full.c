71ee9481c253fc533a5b663aa7c9e64b049a5575
RobberBadeia <robber.badeia@gmail.com>
2024-01-03 20:35:24 +0200

    T15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,7 +7,7 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (0);
+		return (1);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	if (tree->left && tree->right)
