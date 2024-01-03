2fbe3485c5e7a4fbda4510b5a2a9b39ec4cafb74
RobberBadeia <robber.badeia@gmail.com>
2024-01-03 13:11:14 +0200

    T09

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Entry point
+ * @tree: input
+ * Return: tree hight
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_side = 0;
+    size_t right_side = 0;
+
+    if (!tree)
+        return (0);
+    else
+    {
+        left_side = binary_tree_height(tree->left);
+        right_side = binary_tree_height(tree->right);
+        if (left_side > right_side)
+            return (left_side + 1);
+        else
+            return (right_side + 1);
+    }
+}
