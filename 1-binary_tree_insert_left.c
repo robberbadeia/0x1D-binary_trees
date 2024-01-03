b4523a32b88a44cef2824c309a2f8fed0f2fe90c
RobberBadeia <robber.badeia@gmail.com>
2024-01-03 10:57:46 +0200

    T01

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node;
 
 	new_node = binary_tree_node(parent, value);
 	if (!new_node)
