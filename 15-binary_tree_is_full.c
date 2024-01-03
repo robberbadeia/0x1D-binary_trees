7fd28b42b8fe3224e8460979ff28812919306992
RobberBadeia <robber.badeia@gmail.com>
2024-01-03 20:26:48 +0200

    T15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - Entry point
+ * @tree: input
+ * Return: int
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (1);
+}
