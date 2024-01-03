bc7b21eec077928254366ea6e0af273b0a88aa58
RobberBadeia <robber.badeia@gmail.com>
2024-01-03 15:20:27 +0200

    HDR upd

--- binary_trees.h
+++ binary_trees.h
@@ -47,5 +47,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 /* Function that measures the height of a binary tree */
 size_t binary_tree_height(const binary_tree_t *tree);
+/* Function that measures the depth of a node in a binary tree */
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 #endif
