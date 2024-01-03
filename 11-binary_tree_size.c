b03148939221d18f5de4b6aa266720f25b05348d
RobberBadeia <robber.badeia@gmail.com>
2024-01-03 19:34:18 +0200

    T11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - Entry point
+ * @tree: input
+ * Return: binary tree size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size, right_size;
+
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+	return (left_size + right_size + 1);
+}
