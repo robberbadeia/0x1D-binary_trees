d8467cd95b5ddae2e6817b936e6d8705d7bb44cf
RobberBadeia <robber.badeia@gmail.com>
2024-01-03 13:21:34 +0200

    T09

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,17 +13,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	else
+	left_side = binary_tree_height(tree->left);
+	right_side = binary_tree_height(tree->right);
+	if (left_side >= right_side)
 	{
-		left_side = binary_tree_height(tree->left);
-		right_side = binary_tree_height(tree->right);
-		if (left_side >= right_side)
-		{
-			return (left_side + 1);
-		}
-		else
-		{
-			return (right_side + 1);
-		}
+		return (left_side + 1);
 	}
+	return (right_side + 1);
 }
