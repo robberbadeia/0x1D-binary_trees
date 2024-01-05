a7bbac6089d34a840fa820d543620a4c421fe6ff
RobberBadeia <robber.badeia@gmail.com>
2024-01-05 14:28:50 +0200

    HDR

--- binary_trees.h
+++ binary_trees.h
@@ -57,4 +57,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 /* Function that finds the sibling of a node */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+/* Function that finds the uncle of a node */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+
 #endif
