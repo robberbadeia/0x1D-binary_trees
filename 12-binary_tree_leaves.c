a310d887c3bb39e3188e32a042c6a07d6a630d6d
RobberBadeia <robber.badeia@gmail.com>
2024-01-05 14:41:57 +0200

    T12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - Entry point
+ * @tree: input
+ * Return: size_t
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left && tree->right)
+		return (2);
+	return (1);
+}
